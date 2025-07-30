5c485d4939b60f45f93a39bc89c67d45b63812ab
Selleryd <178099989+Selleryd@users.noreply.github.com>
2025-07-30 11:04:00 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-\document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const btn      = document.getElementById('verifyBtn');
   const resultEl = document.getElementById('result');
   const nameEl   = document.getElementById('productName');
